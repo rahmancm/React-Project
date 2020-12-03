8e78717babd4ebfc8627d46605d0b93d6c9d79fa
rahmancm <arahmancm@gmail.com>
2020-12-04 02:13:34 +0530

    React Books

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom";
+//CSS
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const books = [
+  {
+    img:
+      "https://res.cloudinary.com/rahmancm/image/upload/v1602853431/6d55fb0617cd48501f95dfc05506c578_naiick.png",
+    description: "A very Good Book by John",
+    title: " React Book That will make you Pro in React",
+    author: "Daiela John",
+  },
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  {
+    img:
+      "https://res.cloudinary.com/rahmancm/image/upload/v1603568140/png-transparent-web-development-software-developer-web-developer-computer-icons-world-wide-web-web-design-logo-sign_o5pwzq.png",
+    description: "Book That will make you High",
+    title: " High Book Shelf",
+    author: "Theresa mann ",
+  },
+];
+
+function BookList() {
+  return (
+    <section>
+      <h1 className="header"> Free books online </h1>
+      <hr />
+      {books.map((book) => {
+        return <Book book={book}></Book>;
+      })}
+    </section>
+  );
+}
+
+const Book = (props) => {
+  const { img, title, description, author } = props.book;
+  return (
+    <article>
+      <strong>
+        <img src={img} alt="iamhge" />
+        <br />
+        <h1>{title}</h1>
+        <br />
+        <p>{description} </p>
+        <br />
+        <a href="author">{author}</a>
+      </strong>
+    </article>
+  );
+};
+ReactDom.render(<BookList />, document.getElementById("root"));
